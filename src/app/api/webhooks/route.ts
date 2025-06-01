8014865f5fc77761ac9906312b9dde49324f4dd8
Kush <tanishqs1403@gmail.com>
2025-06-01 23:48:35 +0530

    Performing operations on user_data

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,5 +1,6 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
+import axios from 'axios'
 
 export async function POST(req: NextRequest) {
   try {
@@ -9,8 +10,13 @@ export async function POST(req: NextRequest) {
     // For this guide, log payload to console
     const { id } = evt.data
     const eventType = evt.type
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-    console.log('Webhook payload:', evt.data)
+    
+    await axios.post("https://uptimechecker-be.onrender.com/api/syncUsers",{
+        user_data: evt.data
+    })
+
+
+    // console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
 
     return new Response('Webhook received', { status: 200 })
   } catch (err) {
